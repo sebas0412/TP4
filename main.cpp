acdf206ddffe608d08013c72c4af06695c122b47
Ivan Javier Chavarria Vega <b72097@ecci.ucr.ac.cr>
2018-11-16 07:52:56 -0600

    a

--- main.cpp
+++ main.cpp
@@ -2,27 +2,65 @@
 
 using namespace std;
 
+class template<class T>
 class RedNeuronal{
+    Capa capas;
+    Lista capas;
 
+    RedNeuronal(){
+        
+    }
+    void agregarCapa(int cNeuronas){
+        
+    }
+    T Clasificar(int dato){
+        
+    }
+    
+    void Entrenar(int datos){
+        
+    }
+
+    
+       
+    
 };
 
 class Neurona{
-
+    float carga;
+    float umbral;
+    Conexion conexion;
+    Lista conexion;
+    void agregarConexion(neurona, peso){
+        
+    }
+    
+    void aplicarCarga(carga){
+        
+    }
 
 };
 
 class Capa{
-
-
+    Neurona neuronas;
+    Lista neuronas;
+    void agregarNeurona(neuronas){
+        
+    }
 };
 
 class Conexion{
-
+    Neurona r;
+    float peso;
+    void Conexion(Neurona r2, float peso2){
+        r = r2;
+        peso = peso2;
+    }
 
 };
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //cout << "Hello World!" << endl;
     return 0;
 }
