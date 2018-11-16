be5ba3fdd2afcea174cfd7b48c4c83e51222ec4c
IvanCh123 <40545827+IvanCh123@users.noreply.github.com>
2018-11-16 07:51:17 -0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+class RedNeuronal{
+
+};
+
+class Neurona{
+
+
+};
+
+class Capa{
+
+
+};
+
+class Conexion{
+
+
+};
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    return 0;
+}
